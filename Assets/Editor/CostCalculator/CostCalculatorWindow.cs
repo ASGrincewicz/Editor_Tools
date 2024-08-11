12d20e348eaf0f3e8406fa8a05ff04ef4f4fabce
Aaron S Grincewicz <AaronSG@Outlook.com>
2024-08-11 12:37:58 -0400

    Wrote Pseudocode for Cost Calc Window

--- Assets/Editor/CostCalculator/CostCalculatorWindow.cs
+++ Assets/Editor/CostCalculator/CostCalculatorWindow.cs
@@ -1,19 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
-using UnityEngine;
 
-public class CostCalculatorWindow : EditorWindow
+namespace Editor.CostCalculator
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CostCalculatorWindow : EditorWindow
     {
-        
-    }
+        // Constants
+    
+        // Class Variables
+    
+        // GUI Variables
+    
+        [MenuItem("Tools/Utilities/Cost Calculator")]
+        public static void ShowWindow()
+        {
+            CostCalculatorWindow window = (CostCalculatorWindow)EditorWindow.GetWindow(typeof(CostCalculatorWindow));
+            window.Show();
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        private void OnGUI()
+        {
+            // Draw Main Area
+            // Draw  Card Object Field
+            // Draw Card Info Area
+            // Draw Calculation Area
+            // Draw Control Buttons
         
+        }
+
+        /// <summary>
+        /// Draws the main area of the CostCalculatorWindow.
+        /// </summary>
+        /// <remarks>
+        /// This method is responsible for drawing the main area of the CostCalculatorWindow.
+        /// It draws a rectangle with dimensions of 700 width and 700 height.
+        /// </remarks>
+        private void DrawMainArea()
+        {
+            // Draw Main Area Rect 
+            // 700w * 700h
+        }
+
+        /// <summary>
+        /// Draws the card object field in the CostCalculatorWindow.
+        /// </summary>
+        /// <remarks>
+        /// This method is responsible for drawing the card object field in the CostCalculatorWindow.
+        /// It allows the user to select a card object from the Unity editor.
+        /// </remarks>
+        /// <param name="/*START_USER_CODE*/parameterName/*END_USER_CODE*/">/*START_USER_CODE*/The parameter description/*END_USER_CODE*/</param>
+        private void DrawCardObjectField()
+        {
+            // Object Field for loading a card
+        }
+        private void DrawCardInfoArea()
+        {
+            // 600w * 250h
+            // Show values from the loaded card
+        }
+
+        private void DrawCalculationArea()
+        {
+            // 600w * 200h
+            // Display Card Weight Values for the loaded card.
+        }
+
+        private void DrawMessageBox()
+        {
+            // Label for messages
+        }
+
+        private void DrawControlButtons()
+        {
+            // Calculate Cost Button
+            // 200w * 60h
+        
+            // 200w space
+        
+            // Load button
+            // 200w * 60h
+        }
     }
 }
+
+
