18c67618338f0d6a71bd1dcba0d1ecde68229785
ASGrincewicz <AaronSG@outlook.com>
2024-08-25 19:24:48 -0400

    Created basic structure for keyword editor window.

--- Assets/Editor/KeywordSystem/KeywordEditorWindow.cs
+++ Assets/Editor/KeywordSystem/KeywordEditorWindow.cs
@@ -17,20 +17,46 @@ namespace Editor.KeywordSystem
         {
             var window = GetWindow<KeywordEditorWindow>();
             window.titleContent = new GUIContent("Keyword Editor");
-            window.position = new Rect(50, 50, 400, 300);
+            window.position = new Rect(50, 50, 200, 300);
             window.Show();
         }
 
         private void OnGUI()
         {
             SetupAreaRects();
-            // Draw Button Area
-            // Draw Keyword List Area
+            DrawMainArea();
+            DrawButtonArea();
+            DrawKeywordListArea();
         }
 
         private void SetupAreaRects()
         {
-            _mainAreaRect = new Rect(20,5, 400, 300);
+            _mainAreaRect = new Rect(20,5, 200, position.height * 0.40f);
+            _buttonAreaRect = new Rect(20, _mainAreaRect.height + 10, 200, position.height * 0.10f);
+            _keywordListAreaRect = new Rect(20, _buttonAreaRect.height + 10, 200, position.height * 0.50f);
+        }
+
+        private void DrawMainArea()
+        {
+            GUILayout.BeginArea(_mainAreaRect);
+            GUILayout.TextField("Keyword Name:", GUI.skin.textArea);
+            //EditorGUI.IntField("Keyword Value:");
+            // Enum popup field
+            GUILayout.EndArea();
+        }
+
+        private void DrawButtonArea()
+        {
+            GUILayout.BeginArea(_buttonAreaRect);
+            GUILayout.Button("Save");
+            GUILayout.EndArea();
+        }
+
+        private void DrawKeywordListArea()
+        {
+            GUILayout.BeginArea(_keywordListAreaRect);
+            // Get list of all current keywords
+            GUILayout.EndArea();
         }
     }
 }
